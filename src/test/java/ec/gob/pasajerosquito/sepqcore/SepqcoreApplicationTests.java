9cd3f831feb1c0f7a2d00fb4aecc2840047717f3
Roberto Freire <roberto.freire@pasajerosquito.gob.ec>
2024-03-22 00:39:32 -0500

    initial configuration

---
+++ src/test/java/ec/gob/pasajerosquito/sepqcore/SepqcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.gob.pasajerosquito.sepqcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepqcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
